14f3f0a1c17076415e42fe524d4c415be1fbaabe
Shahid <shahid.sarker@gmail.com>
2020-04-19 19:27:36 -0400

    Note form input

--- part2/part2notes/src/App.js
+++ part2/part2notes/src/App.js
@@ -4,6 +4,27 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (e) => {
+    e.preventDefault();
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value);
+    setNewNote(e.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -12,6 +33,10 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
