4d6d526cf82c1fd927a1f60c516e7bde4e57030c
Shahid <shahid.sarker@gmail.com>
2020-04-19 18:44:57 -0400

    Content component renders course parts dynamically

--- part2/course-notes/src/index.js
+++ part2/course-notes/src/index.js
@@ -24,9 +24,9 @@ const Part = (props) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </div>
   );
 };
