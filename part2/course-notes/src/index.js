afed3bd99b15a214292a2958da8632baadf21566
Shahid <shahid.sarker@gmail.com>
2020-04-19 18:41:23 -0400

    Extract Course component

--- part2/course-notes/src/index.js
+++ part2/course-notes/src/index.js
@@ -31,30 +31,41 @@ const Content = ({ course }) => {
   );
 };
 
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course} />
+      <Content course={props.course} />
+    </>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   );
 };
