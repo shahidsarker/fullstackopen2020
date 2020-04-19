a59c884fc6e4c9db67fea6a044f8fcb9559a2a36
Shahid <shahid.sarker@gmail.com>
2020-04-19 19:17:20 -0400

    Implement useState for part2notes

--- part2/part2notes/src/App.js
+++ part2/part2notes/src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
   return (
     <div>
       <h1>Notes</h1>
