acf2a00a55f87a74ebc211ddea999e29a15432f6
Shahid <shahid.sarker@gmail.com>
2020-04-19 19:33:40 -0400

    Notes filter button

--- part2/part2notes/src/App.js
+++ part2/part2notes/src/App.js
@@ -5,6 +5,7 @@ import Note from "./components/Note";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (e) => {
     e.preventDefault();
@@ -21,15 +22,24 @@ const App = (props) => {
   };
 
   const handleNoteChange = (e) => {
-    console.log(e.target.value);
     setNewNote(e.target.value);
   };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
