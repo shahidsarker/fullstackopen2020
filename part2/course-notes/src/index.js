3a8a3102cf77016c44bd2d7d65ba08df81007c43
Shahid <shahid.sarker@gmail.com>
2020-04-19 18:58:36 -0400

    Extract Course component as separate module

--- part2/course-notes/src/index.js
+++ part2/course-notes/src/index.js
@@ -1,47 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return (
-    <p>
-      <strong>total of {sum} exercises</strong>
-    </p>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Course = (props) => {
-  return (
-    <>
-      <Header course={props.course} />
-      <Content course={props.course} />
-      <Total course={props.course} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
