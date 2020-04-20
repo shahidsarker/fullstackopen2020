1137b3e77d807e2973d2f15d18dc8a798a012bf1
Shahid <shahid.sarker@gmail.com>
2020-04-20 15:46:27 -0400

    note importance toggle function

--- part2/part2notes/src/App.js
+++ part2/part2notes/src/App.js
@@ -42,6 +42,18 @@ const App = () => {
     setNewNote(e.target.value);
   };
 
+  const toggleImportanceOf = (id) => {
+    // console.log(`importance of ${id} needs to be toggled`);
+
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
+
   const notesToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true);
@@ -57,7 +69,11 @@ const App = () => {
 
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
