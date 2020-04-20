f4713f55cb0c2148279d3c725d420a53adfa156c
Shahid <shahid.sarker@gmail.com>
2020-04-20 15:53:55 -0400

    Extract notes service

--- part2/part2notes/src/App.js
+++ part2/part2notes/src/App.js
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
+  useEffect(() => {
+    noteService.getAll().then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
     });
-  };
+  }, []);
 
-  useEffect(hook, []);
-  console.log("render", notes.length, "notes");
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
 
   const addNote = (e) => {
     e.preventDefault();
@@ -28,10 +34,9 @@ const App = () => {
       important: Math.random() > 0.5,
     };
 
-    axios
-      .post("http://localhost:3001/notes", noteObject)
+    noteService
+      .create(noteObject)
       .then((response) => {
-        console.log(response);
         setNotes(notes.concat(response.data));
         setNewNote("");
       })
@@ -40,18 +45,6 @@ const App = () => {
 
   const handleNoteChange = (e) => {
     setNewNote(e.target.value);
-  };
-
-  const toggleImportanceOf = (id) => {
-    // console.log(`importance of ${id} needs to be toggled`);
-
-    const url = `http://localhost:3001/notes/${id}`;
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
-
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
-    });
   };
 
   const notesToShow = showAll
