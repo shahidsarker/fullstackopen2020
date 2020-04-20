3ed8513a69d4dd2ddea3d501a37a44d28198b9df
Shahid <shahid.sarker@gmail.com>
2020-04-19 20:10:57 -0400

    Add name to phonebook and check if already exists

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,43 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleAdd = (e) => {
+    e.preventDefault();
+
+    // Check if name is already in phonebook
+    if (persons.find((person) => person.name === newName)) {
+      return alert(newName + " is already in phonebook");
+    }
+
+    const personObj = { name: newName };
+    setPersons(persons.concat(personObj));
+    setNewName("");
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleAdd}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+
+      <div>debug: {newName}</div>
     </div>
   );
 };
