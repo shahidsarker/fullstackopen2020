282d019392061833e5e0fc3cd59db46d77aef0e8
Shahid <shahid.sarker@gmail.com>
2020-04-21 21:44:50 -0400

    requests

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -24,6 +26,26 @@ let notes = [
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const note = notes.find((note) => note.id === id);
+  if (note) res.json(note);
+  else res.status(404).end();
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  console.log(note);
+  res.json(note);
 });
 
 app.get("/api/notes", (req, res) => {
