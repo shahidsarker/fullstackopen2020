916bd091c9ae7e05e28334456dd8bf19bea5d680
Shahid <shahid.sarker@gmail.com>
2020-04-22 22:13:05 -0400

    post generation with error handling, basic

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -42,8 +42,26 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.post("/api/notes", (req, res) => {
-  const note = req.body;
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
   console.log(note);
   res.json(note);
 });
